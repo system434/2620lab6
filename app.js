00c4fe5d755d454ab3d5c0186212da6e233006f3
Jolly <78928544+Jollysquire@users.noreply.github.com>
2021-10-25 22:36:25 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+/*
+ Authors:
+ Your name and student #:
+ Your Partner's Name and student #:
+ (Make sure you also specify on the Google Doc)
+*/
+const express = require("express");
+
+let app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => res.render("pages/index"));
+
+app.get("/myForm", (req, res) => res.render("pages/myForm"));
+
+app.post("myForm", (req, res) => {
+  // Add your implementation here 
+});
+
+app.get("/myListQueryString", (req, res) => {
+  // Add your implementation here
+});
+
+app.get("/search/:movieName", (req, res) => {
+  // Add your implementation here
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 🚀");
+});
