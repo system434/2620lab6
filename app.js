6cb6ff07b0885bafb715015f091d3e35058f44d1
Jolly <78928544+Jollysquire@users.noreply.github.com>
2021-10-30 05:50:46 +0000

    (Imagine making frequent pushes) Add myForm text area that updates movie list. Add get query function to display movies from search bar.

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 /*
  Authors:
- Your name and student #:
- Your Partner's Name and student #:
- (Make sure you also specify on the Google Doc)
+ Your name and student #: James Murphy - A01236645
+ Your Partner's Name and student #: 
 */
 const express = require("express");
 
+let movieDicts = []
 let app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 
-app.get("/", (req, res) => res.render("pages/index"));
+app.get("/", (req, res) => res.render("pages/index",  { movieList: movieDicts }));
 
 app.get("/myForm", (req, res) => res.render("pages/myForm"));
 
-app.post("myForm", (req, res) => {
-  // Add your implementation here 
+app.post("/myForm", (req, res) => {
+  movieStr = req.body.movieStr
+  movieList = movieStr.split(',')
+  for(movie of movieList){
+    movieDicts.push({name: `${movie}`})
+  }
+  res.redirect('/')
 });
 
 app.get("/myListQueryString", (req, res) => {
-  // Add your implementation here
+  fakeMovieDicts = []
+
+  items = req.query
+  console.log
+  for(i in items){
+    fakeMovieDicts.push({name: `${items[i]}`})
+  }
+  res.render("pages/index",  { movieList: fakeMovieDicts })
 });
 
 app.get("/search/:movieName", (req, res) => {
